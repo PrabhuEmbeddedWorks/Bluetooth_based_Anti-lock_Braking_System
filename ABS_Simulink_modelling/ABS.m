07b5b49de44170fafcd291a95ed7edb723509196
Basavaprabhu <prabhu26ec@gmail.com>
2025-09-04 21:22:27 +0530

    Add files via upload

---
+++ ABS_Simulink_modelling/ABS.m
@@ -0,0 +1,7 @@
+g=9.8100; % gravitational acceleration
+I=5; % moment of inertia of the wheel
+m=75; % mass of the vehicle
+R=1.2500; % wheel radius
+v0=44; % initial vehicle velocity (Vehicle speed) %stop time 25 sec
+
+
